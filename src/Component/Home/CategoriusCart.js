4aab9ca036e45f0991128794f3465222549941b3
unknown <abdulkaderjilani120@gmail.com>
2022-11-13 21:09:51 +0600

    reponsive issue is solved

--- src/Component/Home/CategoriusCart.js
+++ src/Component/Home/CategoriusCart.js
@@ -6,9 +6,10 @@ const CategoriusCart = () => {
     <div className='categorious-main' style={{
       backgroundImage: `url("https://images.unsplash.com/photo-1588756681780-9d5859fc2ca0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80")`
     }}>
-      <div className="categorious-overflow ">
-        <div className=''>
-          <div className="row pb-5 ">
+     
+      <div className="categorious-overflow">
+        <div className='container'>
+          <div className="row  ">
             <div className="col-md-6 col-lg-3 ">
               <div className="card cate-card">
                 <p className='cate-card-title'>SPORTBIKE</p>
